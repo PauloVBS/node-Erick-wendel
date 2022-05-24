f7f7070d469ada0af141c66fa06efa8b412ea193
slasher314 <paulo.vb.souza@unesp.br>
2022-05-24 13:30:09 -0300

    parte 6

---
+++ aula5/01/tests.js
@@ -0,0 +1,22 @@
+const assert = require('assert')
+const{
+    obterPessoas
+}= require('../../6-cli/service')
+
+const nock = require('nock')
+
+describe ("Star Wars Tests", function(){
+    this.beforeAll(()=>{
+        const response={
+
+        }
+    })
+    it('deve buscar o r2d2 com o formato correto', async () =>{
+        const expected = [{
+            nome:'R2-D2', 
+            peso: '96'}]
+        const nomeBase ='r2-d2'
+        const resultado = await obterPessoas(nomeBase)
+        assert.deepEqual(resultado, expected)
+    })
+})
